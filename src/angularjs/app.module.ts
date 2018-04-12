08996889216366dc769ffe0075a8029a025a09d0
Josue Zarzosa <josketres@gmail.com>
2018-04-12 20:21:50 +0200

    add placeholder layout loader

--- src/angularjs/app.module.ts
+++ src/angularjs/app.module.ts
@@ -1,7 +1,7 @@
 export const ng1App: any = angular.module('ng1App', []);
 
 ng1App.component('header', {
-    template: '<div style="border: 3px solid red;padding: 3px;">{{$ctrl.message}}</div>',
+    template: '<div class="ng1-header">{{$ctrl.message}}</div>',
     controller: function () {
         this.message = 'Angular 1 header';
     }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
-        <div style="border: 3px solid blue;padding: 3px;margin-top: 3px;">
+        <div style="background:#e1dfdd;padding: 10px;margin: 10px;min-height:100vh;">
             <router-outlet></router-outlet>
             <p>
                 This app demonstrates code-splitting, lazy-loading and aot-compilation for hybrid projects.
